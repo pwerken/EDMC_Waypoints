1bcda71d143259bc007797964ac2612aa39251c1
Peter van de Werken <peter@van-de-werken.nl>
2022-02-09 17:41:01 +0100

    shuffle code

--- load.py
+++ load.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import sys
 
 from waypoints import Waypoints
--- waypoints.py
+++ waypoints.py
@@ -4,14 +4,20 @@ from plugin_gui import PluginGui
 
 class Waypoints:
 
+    _gui = None
+    _route = list()
+    _save_route = None
+
     def __init__(self, plugin_dir):
-        self._gui = None
-        self._route = []
         self._save_route = os.path.join(plugin_dir, 'save_route.txt')
         self.load(self._save_route)
 
     def __len__(self):
         return len(self._route)
+
+    def clear(self):
+        self._route.clear()
+        self.save()
 
     def create_ui(self, parent):
         if self._gui is None:
@@ -30,19 +36,12 @@ class Waypoints:
         self.save()
         self._gui.update_ui()
 
-    def clear(self):
-        self._route = []
-        self._nearest = None
-        if os.path.isfile(self._save_route):
-            os.remove(self._save_route)
-
     def load(self, filename):
-        if len(filename) == 0:
-            return False
-        if not os.path.isfile(filename):
+        if len(filename) == 0 or not os.path.isfile(filename):
             return False
 
         try:
+            self._route.clear()
             with open(filename, 'r') as f:
                 for line in f:
                     clean = line.rstrip(' \r\n').replace('"', '')
@@ -50,22 +49,25 @@ class Waypoints:
 
                     if len(s[0]) == 0 or s[0][0] == '#':
                         continue
-                    if s[0] == 'System Name':
+                    if s[0].lower() == 'system name':
                         continue
                     self._route.append(s[0])
             self.save()
+            return True
         except IOError:
             print("Failed to read file {}".format(filename))
-            self._route = []
+            self.clear()
             return False
 
-        return True
-
     def save(self):
+        if len(self) == 0:
+            if os.path.isfile(self._save_route):
+                os.remove(self._save_route)
+            return
         try:
             with open(self._save_route, 'w') as f:
                 f.write('System Name\n')
-                for i in range(0, len(self._route)):
+                for i in range(0, len(self)):
                     f.write(self._route[i])
                     f.write('\n')
         except IOError:
